fda92cd9032acc434168860db4a5cf752a2f0b45
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-02 23:35:08 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/animatedsplashscreen/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.animatedsplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
