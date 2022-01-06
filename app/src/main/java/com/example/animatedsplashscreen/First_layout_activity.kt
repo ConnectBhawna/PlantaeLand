062137f4dcea89c2096ae0f9de651e6afce7028e
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-06 15:51:39 +0530

    scroable view

--- app/src/main/java/com/example/animatedsplashscreen/First_layout_activity.kt
+++ app/src/main/java/com/example/animatedsplashscreen/First_layout_activity.kt
@@ -2,15 +2,51 @@ package com.example.animatedsplashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class First_layout_activity : AppCompatActivity() {
+
+    private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.plantcare)
 
+
         val actionBar = supportActionBar
 
-        actionBar!!.title = "FirstLayoutActivity"
-        actionBar.setDisplayHomeAsUpEnabled(true)
+//        actionBar!!.title = "FirstLayoutActivity"
+
+//        actionBar.setDisplayHomeAsUpEnabled(true)
+//github me code push karo and muje link send karo okk doing
+        
+
+        setOnboardingItem()
+    }
+
+    private fun setOnboardingItem(){
+        onboardingItemsAdapter = OnboardingItemsAdapter(
+            listOf(
+                OnboardingItem(
+                    onboardingImages = R.drawable.pic1,
+                    title = "Step 1",
+                    description = "Explore plant care guides"
+                ),
+                OnboardingItem(
+                        onboardingImages = R.drawable.pic2,
+                        title = "Step 2",
+                        description="Get timely care reminders"
+
+                ),
+                OnboardingItem(
+                    onboardingImages = R.drawable.pic3,
+                    title = "Step 3",
+                    description="Start your own garden"
+
+                )
+
+            )
+        )
+        val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
+        onboardingViewPager.adapter = onboardingItemsAdapter
     }
 }
---
+++ app/src/main/java/com/example/animatedsplashscreen/OnboardingItem.kt
@@ -0,0 +1,11 @@
+package com.example.animatedsplashscreen
+
+import android.accounts.AuthenticatorDescription
+import android.provider.MediaStore
+
+data class OnboardingItem(
+    val onboardingImages: Int,
+    val title: String,
+    val description: String
+
+)
